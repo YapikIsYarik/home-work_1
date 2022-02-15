08cdaadcca2b5e6782f218d0bbb3f7d5e416572a
yaroslav <yaroslavpatcalon@gmail.com>
2022-02-15 16:29:44 +0200

    Поправки відповідно з зауваженнями в Трелло

--- main.js
+++ main.js
@@ -1,56 +1,55 @@
 "use strict"
 
-const oranges = 15.678;
-const apples = 123.965;
-const melons = 90.2345;
+const ORANGES = 15.678;
+const APPLES = 123.965;
+const MELONS = 90.2345;
 //Максимальне число:
-const maxNum = Math.max(oranges, apples, melons);
-console.log(`Максимальне число: ${maxNum} `);
+const MAX_NUM = Math.max(ORANGES, APPLES, MELONS);
+console.log(`Максимальне число: ${MAX_NUM} `);
 
 //Мінімальне число
-const minNum = Math.min(oranges, apples, melons)
-console.log(`Манімальне число: ${minNum} `);
+const MIN_NUM = Math.min(ORANGES, APPLES, MELONS)
+console.log(`Манімальне число: ${MIN_NUM} `);
 //Суму всіх товарів
-const priceALL = oranges + apples + melons;
-console.log(`Вартість всіх товарів: ${priceALL}`);
+const PRICE_ALL = ORANGES + APPLES + MELONS;
+console.log(`Вартість всіх товарів: ${PRICE_ALL}`);
 
 //Сума цілих цін товарів(заокруглював до меншого за умовою а потім додавав їх)
-const intOranges = Math.floor(oranges);
-const intApples = Math.floor(apples);
-const intMelons = Math.floor(melons);
-const intPriceAll = intOranges + intApples + intMelons;
-console.log(`Сума товарів без копійок: ${intPriceAll}`);
+const INT_ORANGES = Math.floor(ORANGES);
+const INT_APPLES = Math.floor(APPLES);
+const INT_MELONS = Math.floor(MELONS);
+const INT_PRICE_ALL = INT_ORANGES + INT_APPLES + INT_MELONS;
+console.log(`Сума товарів без копійок: ${INT_PRICE_ALL}`);
 
 //Заокруглення до сотень
-const roundedSum = +oranges.toFixed(2) + +apples.toFixed(2) + +melons.toFixed(2);
-const finalRoundedSum = Math.round(roundedSum / 100) * 100;
-console.log(`Сума товарів округлена до сотень: ${finalRoundedSum}`);
+const ROUNDED_SUM = +ORANGES.toFixed(2) + +APPLES.toFixed(2) + +MELONS.toFixed(2);
+const FINAL_ROUNDED_SUM = Math.round(ROUNDED_SUM / 100) * 100;
+console.log(`Сума товарів округлена до сотень: ${FINAL_ROUNDED_SUM}`);
 
 //Парність числа
-const even = intPriceAll % 2;
-if (even == 0) {
-   const isTrue = true;
-   console.log(`Парність числа: ${isTrue}`);
+const EVEN = INT_PRICE_ALL % 2;
+if (EVEN == 0) {
+   console.log(`Парність числа: +EVEN`);
 } else {
    console.log("number not even");
 }
 
 //Здача з 500
-const changes = 500 - priceALL;
-console.log(`Здача з 500: ${changes}`);
+const CHANGES = 500 - PRICE_ALL;
+console.log(`Здача з 500: ${CHANGES}`);
 
 //Середнє значення до сотень
-const avaragePrice = ((oranges + apples + melons) / 3).toFixed(2);
-console.log(`Середнє значення цін до сотень: ${avaragePrice}`);
+const AVARAGE_PRICE = ((ORANGES + APPLES + MELONS) / 3).toFixed(2);
+console.log(`Середнє значення цін до сотень: ${AVARAGE_PRICE}`);
 
 //рандомна знижка від 0 до 100%
-const discount = Math.round(Math.random() * 100);
-console.log(`Рандомна знижка: ${discount + "%"}`);
+const DISCOUNT = Math.round(Math.random() * 100);
+console.log(`Рандомна знижка: ${DISCOUNT + "%"}`);
 
 //Ціна товару зі знижкою
-const discountedPay = priceALL - (priceALL * (discount / 100));
-console.log(`Ціна товару з рандомною знижкою ${discountedPay.toFixed(2)}`);
+const DISCOUNTED_PAY = PRICE_ALL - (PRICE_ALL * (DISCOUNT / 100));
+console.log(`Ціна товару з рандомною знижкою ${DISCOUNTED_PAY.toFixed(2)}`);
 
 //Чистий прибуток
-const netProfit = (priceALL / 2) - (priceALL * (discount / 100));
-console.log(`Чистий прибуток: ${netProfit}`);
+const NET_PROFIT = (PRICE_ALL / 2) - DISCOUNTED_PAY;
+console.log(`Чистий прибуток: ${NET_PROFIT}`);
